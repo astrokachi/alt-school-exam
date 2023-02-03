d98f5dee779d03dae5a9d1e5038c951bfc1ea2e8
Kachi <astrokachi@gmail.com>
2023-02-03 04:09:31 +0100

    updated routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,29 +1,30 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 const blogRouter = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const {
-  getAllBlogs,
-  postBlog,
-  updateBlogPost,
-  deleteBlogPost,
-  getBlog,
-  getMyBlogs,
-} = require('../controllers/blogs');
+	getAllBlogs,
+	postBlog,
+	updateBlogPost,
+	deleteBlogPost,
+	getBlog,
+	getMyBlogs,
+} = require("../controllers/blogs");
+
+blogRouter.route("/").get(getAllBlogs);
 
 blogRouter
-  .route('/')
-  .get(getAllBlogs)
-  .post(passport.authenticate('jwt', { session: false }), postBlog);
+	.route("/create")
+	.post(passport.authenticate("jwt", { session: false }), postBlog);
 
 blogRouter
-  .route('/my_blogs')
-  .get(passport.authenticate('jwt', { session: false }), getMyBlogs);
+	.route("/my_blogs")
+	.get(passport.authenticate("jwt", { session: false }), getMyBlogs);
 
 blogRouter
-  .route('/:id')
-  .get(getBlog)
-  .patch(passport.authenticate('jwt', { session: false }), updateBlogPost)
-  .delete(passport.authenticate('jwt', { session: false }), deleteBlogPost);
+	.route("my_blogs/:id")
+	.get(getBlog)
+	.patch(passport.authenticate("jwt", { session: false }), updateBlogPost)
+	.delete(passport.authenticate("jwt", { session: false }), deleteBlogPost);
 
 module.exports = blogRouter;
