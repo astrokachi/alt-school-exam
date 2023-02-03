8d5853b0ae8ef28c2f40b90178b5c1c83688d6b8
Kachi <astrokachi@gmail.com>
2023-02-03 04:33:36 +0100

    added security updates

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ require("./middleware/authentication");
 require("express-async-errors");
 const authRouter = require("./routes/auth");
 const blogRouter = require("./routes/blogs");
-const cors = require('cors')
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.use("/auth", authRouter);
 app.use("/blogs", blogRouter);
